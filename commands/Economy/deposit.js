2877e5a0db3b6ab002240cca8b9596a3e913fa28
Drishtant Ghosh <ggdrishtant@gmail.com>
2023-01-16 16:14:09 +0530

    shifting to mongo `75%` complete

--- commands/Economy/deposit.js
+++ commands/Economy/deposit.js
@@ -1,84 +1,80 @@
+const {
+  EmbedBuilder,
+  PermissionsBitField,
+  ApplicationCommandOptionType,
+  MessageEmbed,
+} = require("discord.js");
 
-const { EmbedBuilder, PermissionsBitField, ApplicationCommandOptionType } = require("discord.js");
-const CurrencySystem = require("currency-system");
-const cs = new CurrencySystem;
+const User = require("../../Models/User.js");
+
 module.exports = {
-    name: "deposit",
-    description: "Deposits a given Amount to Bank",
-  category:'economy',
-    aliases:['dep'],
-  cooldown: 5,
+  name: "deposit",
 
-   
-    run: async (client, message, args, prefix) => {
-         let money = args[0];
-        const crs = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-    
-        .setDescription(`<:11:1052589045374533653> Enter A Valid Amount`)
-  if (!money) return message.reply({embeds:[crs]});
-        const user = message.author;
-        let result = await cs.deposite({
-            user: user,
-            guild:{id: null},
-            amount: money,
-        });
-      
-        if (result.error) {
-    if (result.type === "money"){
-        const wrong = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-        .setDescription(`<:11:1052589045374533653> Enter an Amount to deposite first.`)
-      return message.reply({embeds: [wrong]})
-    }
-    if (result.type === "negative-money"){
-        const nm = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-        .setDescription(`<:11:1052589045374533653> You Can\'t Deposite Negative Money.`)
-      return message.reply({embeds: [nm]})}
-    if (result.type === "low-money"){   
-        const lm = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-        .setDescription(`<:11:1052589045374533653> You Don\'t Have that much money in Your Wallet.`)
-      return message.reply({embeds: [lm]})
-                                    }
-    if (result.type === "no-money"){   
-        const lm = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-        .setDescription(`<:11:1052589045374533653> You Don\'t Have Money in Your Wallet.`)
-      return message.reply({embeds: [lm]})
-                                    }
-      if (result.type === "bank-full"){   
-        const lm = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-        .setDescription(`<:11:1052589045374533653> You\'re Bank is Full.\n It is on its maximum limit.`)
-      return message.reply({embeds: [lm]})
-                                    }
-  } else {
-    if (result.type === "all-success")
-      {    
-        const lms = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-          .setDescription(`
-Deposited all the amount <a:bitcoin:1055862360713220237>${result.amount} To Bank.
-**Updated Balance:**
-<a:wallet:1055761007789748275> Wallet: <a:bitcoin:1055862360713220237>${result.rawData.wallet}
-<:ecobank:1055873821590175784> Bank: <a:bitcoin:1055862360713220237>${result.rawData.bank}`)
-      return message.reply({embeds: [lms]})
-                                    }
-      if (result.type === "success"){
-            const ems = new EmbedBuilder()
-        .setColor(message.guild.members.me.displayHexColor !== '#000000' ? message.guild.members.me.displayHexColor : client.config.embedColor)
-        .setDescription(`
-Deposited <a:bitcoin:1055862360713220237>${result.amount} To Bank.
-**Updated Balance:**
-<a:wallet:1055761007789748275> Wallet: <a:bitcoin:1055862360713220237>${result.rawData.wallet}
-<:ecobank:1055873821590175784> Bank: <a:bitcoin:1055862360713220237>${result.rawData.bank}`)
-           
-      return message.reply({embeds:[ems]})
-  }
-  }
-        
+  description: "Deposit The balance of an user",
 
-    }
-}
+  category: "economy",
+
+  aliases: ["dep"],
+
+  cooldown: 2,
+
+  run: async (client, message, args, prefix) => {
+    const user = message.author;
+    const amount = parseInt(args[0]);
+
+    const result =
+      (await User.findOne({ userId: user.id })) ||
+      new User({ userId: user.id });
+    // run: async (interaction) => {
+
+    const give = new EmbedBuilder()
+
+      .setColor(
+        message.guild.members.me.displayHexColor !== "#000000"
+          ? message.guild.members.me.displayHexColor
+          : client.config.embedColor
+      )
+
+      .setDescription(`<:11:1052589045374533653> Enter A Valid Amount`);
+
+    if (!amount) return message.reply({ embeds: [give] });
+
+    const crs = new EmbedBuilder()
+
+      .setColor(
+        message.guild.members.me.displayHexColor !== "#000000"
+          ? message.guild.members.me.displayHexColor
+          : client.config.embedColor
+      )
+
+      .setDescription(
+        `💰 You need \` ${
+          amount - result.wallet
+        } 🪙 \` more in your wallet to deposit money`
+      );
+
+    if (result.wallet < amount) return message.reply({ embeds: [crs] });
+
+    // embed = new MessageEmbed({ color: "YELLOW" })
+
+    const success = new EmbedBuilder()
+
+      .setColor(
+        message.guild.members.me.displayHexColor !== "#000000"
+          ? message.guild.members.me.displayHexColor
+          : client.config.embedColor
+      )
+
+      .setDescription(
+        `✅ You have deposited \` ${amount} 🪙 \` amount into your bank account`
+      );
+
+    result.wallet -= amount;
+
+    result.bank += amount;
+
+    result.save();
+
+    return message.reply({ embeds: [success] });
+  },
+};
--- commands/Economy/groupgamble.js
+++ commands/Economy/groupgamble.js
@@ -1,85 +1,100 @@
-const { EmbedBuilder, PermissionsBitField, ApplicationCommandOptionType } = require("discord.js");
-
+const {
+  EmbedBuilder,
+  PermissionsBitField,
+  ApplicationCommandOptionType,
+  ActionRowBuilder,
+  ButtonBuilder,
+} = require("discord.js");
+const User = require("../../Models/User.js");
 module.exports = {
-    name: "groupgamble",
-    description: "Gamble with multiple users",
-  category:'economy',
+  name: "groupgamble",
+  description: "Gamble with multiple users",
+  category: "economy",
+  aliases: ["groupbet"],
   cooldown: 5,
-  userPerms: ["grpgamble", "grpbet", "groupbet"],
-   
-    run: async (client, message, args, prefix) => {
+  userPerms: [""],
 
-        let amount = interaction.options.getInteger('amount')
-        if (amount < 0) return interaction.followUp(`Provide valid amount to start **group** gamble !`)
-        let button = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setLabel("JOIN")
-                    .setStyle("SUCCESS")
-                    .setCustomId("accept"))
-        let buttonx = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setLabel("JOIN")
-                    .setStyle("SUCCESS")
-                    .setCustomId("disabledAccept")
-                    .setDisabled()
-            )
-        let participants = ` `
-        let txt = `Click the button to join group gamble ! ( 30s to join. )\n**Amount**: \`${amount}\` Credits\n\`\`\`Participants:\n${participants}\n\`\`\``
-        let msg = await interaction.followUp({ content: `${txt}`, components: [button] })
+  run: async (client, message, args, prefix) => {
+    let amount = parseInt(args[0]);
+    if (amount < 0)
+      return message.channel.send(`You can't gamble less than 0 credits !`);
+    if (!amount) return message.channel.send(`Enter an amount to gamble !`);
+    if (isNaN(amount))
+      return message.channel.send(`That's not a valid number !`);
+    if (amount && !isNaN(amount)) {
+      let button = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel("Join")
+          .setStyle("Success")
+          .setCustomId("accept")
+      );
+      let buttonx = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel("Join")
+          .setStyle("Success")
+          .setCustomId("disabledAccept")
+          .setDisabled(true)
+      );
+      let participants = ` `;
+      let txt = `Click the button to join group gamble ! ( 30s to join. )\n**Amount**: \`${amount}\` Credits\n\`\`\`Participants:\n${participants}\n\`\`\``;
+      const msg = await message.channel.send({
+        //const msg = message.followUp({
+        content: `${txt}`,
+        components: [button],
+      });
 
-        const filter = async (inter) => {
-            let userx = await User.findOne({ id: inter.user.id })
-            if (userx && userx.balance >= amount) return true
-            else if (!userx) {
-                await inter.deferUpdate()
-                inter.followUp({
-                    content: `<@${inter.user.id}> , you need to pick a starter pokémon using the \`${prefix}start\` command before using this command !`,
-                    ephemeral: true
-                })
-                return false
-            } else if (userx.balance < amount) {
-                await inter.deferUpdate()
-                inter.followUp({
-                    content: `<@${inter.user.id}> , you don't have enough balance to join group gamble !`,
-                    ephemeral: true
-                })
-                return false
-            }
+      const filter = async (inter) => {
+        let userx =
+          (await User.findOne({ userId: inter.user.id })) ||
+          new User({ userId: inter.user.id });
+        if (userx && userx.wallet >= amount) return true;
+        else if (userx.wallet < amount) {
+          await inter.reply({
+            content: `You don't have enough balance to join group gamble !`,
+            ephemeral: true,
+          });
+          return false;
         }
-        const collector = await msg.createMessageComponentCollector({
-            filter, time: 30000
-        })
-        let allPlayers = [], random
-        collector.on('collect', async i => {
-            await i.deferUpdate()
-            if (i.customId === 'accept' && !allPlayers.includes(i.user.id)) {
-                participants += `\n${i.user.tag}`
-                txt = `Click the button to join group gamble ! ( 30s to join. )\n**Amount**: \`${amount.toLocaleString()}\` Credits\n\`\`\`\nParticipants:${participants}\n\`\`\``
-                msg.edit({ content: `${txt}` })
-                allPlayers.push(i.user.id)
-            } else {
-                return i.followUp({
-                    content: `<@${interaction.user.id}> , you have already joined the group gamble !`,
-                    ephemeral: true
-                })
-            }
-        })
-        collector.on('end', async () => {
-            interaction.editReply({ components: [buttonx] })
-            if (allPlayers.length <= 2) return interaction.channel.send({ content: `Group gamble cancelled due to insufficient participants !` })
-            random = ~~(Math.random() * allPlayers.length)
-            for (let i = 0; i < allPlayers.length; i++) {
-                let userx = await User.findOne({ id: allPlayers[i] })
-                if (allPlayers[random] == allPlayers[i]) userx.balance += amount
-                else userx.balance -= amount
-                await userx.save()
-            }
-            return interaction.channel.send(`<@${allPlayers[random]}> won \`${amount.toLocaleString()}\` credits !`)
-        })
-        
-     
+      };
+      const collector = await msg.createMessageComponentCollector({
+        filter,
+        time: 30000,
+      });
+      let allPlayers = [];
+      collector.on("collect", async (i) => {
+        await i.deferUpdate();
+        if (i.customId === "accept" && !allPlayers.includes(i.user.id)) {
+          participants += `\n${i.user.tag}`;
+          txt = `Click the button to join group gamble ! ( 30s to join. )\n**Amount**: \`${amount.toLocaleString()}\` Credits\n\`\`\`\nParticipants:${participants}\n\`\`\``;
+          msg.edit({ content: `${txt}` });
+          allPlayers.push(i.user.id);
+        } else {
+          return i.reply({
+            content: `You have already joined the group gamble !`,
+            ephemeral: true,
+          });
+        }
+      });
 
+      collector.on("end", async () => {
+        msg.edit({ components: [buttonx] });
+        if (allPlayers.length <= 2)
+          return message.channel.send({
+            content: `Group gamble cancelled due to insufficient participants !`,
+          });
+        random = ~~(Math.random() * allPlayers.length);
+        for (let i = 0; i < allPlayers.length; i++) {
+          let winamount = amount * allPlayers.length;
+          let userx = await User.findOne({ userId: allPlayers[i] });
+          if (allPlayers[random] == allPlayers[i]) userx.wallet += winamount;
+          else userx.wallet -= amount;
+          await userx.save();
+        }
+        let winamount = amount * allPlayers.length;
+        return message.channel.send(
+          `<@${allPlayers[random]}> won \`${winamount.toLocaleString()}\` credits !`
+        );
+      });
     }
-}
+  },
+};
