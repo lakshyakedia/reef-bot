9c5d5c8a3f17e262bfeb37da069a35a416f6c612
Drishtant Ghosh <ggdrishtant@gmail.com>
2024-02-20 09:26:48 +0530

    Update stats.js

--- SlashCommands/Utility/stats.js
+++ SlashCommands/Utility/stats.js
@@ -42,21 +42,20 @@ run : async (client,interaction,args) => {
   const embed = new EmbedBuilder()
         
   .setColor(`${client.config.embedColor}`)
-  .setAuthor({ name: "Bot Information", iconURL: client.user.displayAvatarURL(), url: "https://discord.gg/bothub"})
-  .setDescription(`What's up ${interaction.user.username}! I am ${client.user.username}, a cool discord music bot for your server!`)
+  .setAuthor({ name: "Bot Information", iconURL: client.user.displayAvatarURL(), url: "https://discord.gg/reefbot"})
+  .setDescription(`What's up ${interaction.user.username}! I am ${client.user.username}, a cool discord bot!`)
   .addFields({name:`<:stats:985100769520930816> Stats`,value:
   `➜ **${client.guilds.cache.size}** Servers\n➜ **${users}** Users\n➜ **${client.channels.cache.size}** Channels\n`},
   {name:`Command Used By You`,value:`${data.count}Commands`},
 
   {name:`<:links:985105785921081344> Links`,value:
-`➜ [Invite](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands)\n➜ [Support Server](https://discord.gg/wrCzESkVzK)`},
+`➜ [Invite](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands)\n➜ [Support Server](https://discord.gg/reefbot)`},
   {name:`<:gi:985100765783810151> General Information`,value:
   `➜ Connections: **${connectedchannelsamount}**\n➜ Platform: \`${os.platform()}\`\n➜ CPU Usage:\`${(process.cpuUsage().system / 1024 / 1024).toFixed(2)}%\`\n➜ RAM Usage: \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}Mb\`\n➜ Ping: \`${client.ws.ping}ms\`\n➜ Up Since: <t:${duration1}:R> `},
   
-  {name:`<:dev:978563383580295188> Developers`,value:`\`\`\`js\n! ARYAN.NFT#0082\nBraylon ඞ#5950\`\`\``},
+  {name:`<:dev:978563383580295188> Developers`,value:`\`\`\`js\ndrix10\naryannn\`\`\``},
   {name:`<:djs:984372533002395720> Discord.JS Version`,value:`\`\`\`js\n${discordJSVersion}\`\`\``},
-  {name:`<:nodejs:984372538236891196> Node.JS Version`,value:`\`\`\`js\n${process.version}\`\`\``},
-  {name:`<:premiumbadge:967414570169802832> Poru  Version`,value:`\`\`\`js\n${client.poru.version}\`\`\``})
+  {name:`<:nodejs:984372538236891196> Node.JS Version`,value:`\`\`\`js\n${process.version}\`\`\``})
   .setThumbnail(client.user.displayAvatarURL())
   .setFooter({text:`Thank you for your continued support to ${client.user.username}. <3`})
         interaction.reply({embeds: [embed]})
